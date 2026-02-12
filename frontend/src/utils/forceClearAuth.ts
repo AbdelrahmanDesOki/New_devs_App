383640ef98b2b014237c65a8757702e8ad74c99f
Desokii <desoki.abdelrahman@gmail.com>
2026-02-12 18:58:53 +0400

    current progress

--- frontend/src/utils/forceClearAuth.ts
+++
@@ -1,7 +0,0 @@
-// Global utility to force clear authentication
-(window as any).forceClearAuth = () => {
-  localStorage.clear();
-  sessionStorage.clear();
-  window.location.reload();
-  console.log('Authentication forcefully cleared');
-};
